b91ec806884955e1db0a9a53fefa0d5cd8b4687f
Liljack118 <kamilzajaczek4@gmail.com>
2023-04-03 19:45:30 +0200

    export table to zip file

--- src/file_export.py
+++ src/file_export.py
@@ -1,5 +1,6 @@
 from google.cloud import bigquery
 from dataclasses import dataclass, field
+from typing import Iterator
 
 
 @dataclass
@@ -9,7 +10,8 @@ class BigQueryExporter:
     bucket_name: str
     project_id: str | None = field(default=None)
     # init exluded attributes
-    client: None = field(init=False, default=None)
+    client: bigquery.Client | None = field(init=False, default=None)
+    comporess: bool = field(init=False, default=False)
 
     def __post_init__(self):
         self.client = bigquery.Client()
@@ -27,6 +29,8 @@ class BigQueryExporter:
         job_config.destination_format = (
             bigquery.DestinationFormat.NEWLINE_DELIMITED_JSON
         )
+        if self.comporess:
+            job_config.compression = bigquery.Compression.GZIP
         return job_config
 
     def export_file(self, destination_uri: str) -> None:
@@ -45,14 +49,24 @@ class BigQueryExporter:
     def export_to_csv(self, destination_uri: str) -> None:
         self.export_file(destination_uri)
 
-    def export(self, file_name: str, formats: None | list[str] = None) -> None:
+    def export(
+        self, file_name: str, formats: None | list[str] = None, compress=False
+    ) -> None:
         if formats is None:
             formats = []
 
-        for f in formats:
-            destination_uri = f"gs://{self.bucket_name}/{file_name}.{f}"
-            export_func = getattr(self, f"export_to_{f}")
+        self.comporess = compress
+
+        for save_f, norm_f in self._normalized_formats(formats):
+            destination_uri = f"gs://{self.bucket_name}/{file_name}.{save_f}"
+            export_func = getattr(self, f"export_to_{norm_f}")
             export_func(destination_uri)
+
+    def _normalized_formats(self, formats: list[str]) -> Iterator[str]:
+        comporess_ext = "" if not self.comporess else ".gz"
+        for f in formats:
+            f = f.split(".", 1)[0]
+            yield f"{f}{comporess_ext}", f
 
 
 def main():
@@ -62,4 +76,4 @@ def main():
         table_id="bikeshare_stations",
         bucket_name="bigquery-bucket-123123",
     )
-    exporter.export(file_name="bigquery-public-data", formats=["json"])
+    exporter.export(file_name="bigquery-public-data", formats=["json"], compress=True)
