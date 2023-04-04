71e318090d05e6aaec57f8817468084cad9f1ddc
Liljack118 <kamilzajaczek4@gmail.com>
2023-04-04 20:47:55 +0200

    connect big query table with google sheets

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 import importlib
+import os
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Please provide a function name")
         sys.exit(1)
 
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "secrets/service-key.json"
     func = sys.argv[1]
     module = importlib.import_module(f"src.{func}")
     module.main()
---
+++ src/connect_with_google_sheets.py
@@ -0,0 +1,55 @@
+from google.cloud import bigquery
+from dataclasses import dataclass
+import google.auth
+import google.oauth2
+import time
+
+
+@dataclass
+class BigQueryConnectedGoogleSheet:
+    sheet_url: str
+    dataset_id: str
+    table_id: str
+
+    def __post_init__(self):
+        credentials, project = google.auth.default(
+            scopes=[
+                "https://www.googleapis.com/auth/drive",
+                "https://www.googleapis.com/auth/bigquery",
+            ],
+        )
+
+        self.client = bigquery.Client(credentials=credentials, project=project)
+
+    @property
+    def dataset(self) -> bigquery.Dataset:
+        return self.client.get_dataset(self.dataset_id)
+
+    @property
+    def external_config(self) -> bigquery.ExternalConfig:
+        external_config = bigquery.ExternalConfig("GOOGLE_SHEETS")
+        external_config.autodetect = True
+        external_config.source_uris = [self.sheet_url]
+        return external_config
+
+    @property
+    def table(self) -> bigquery.Table:
+        table = bigquery.Table(self.dataset.table(self.table_id))
+        table.external_data_configuration = self.external_config
+        return table
+
+    def create_table(self) -> None:
+        """Create table connected to Google Sheets"""
+
+        table = self.client.create_table(self.table)
+
+
+def main():
+    SHEET_URL = "https://docs.google.com/spreadsheets/d/1MxvWi-4AbgXKyjzMc11u3yXcFEFTTWVPr_PJ6o5DzzI/edit#gid=113404796"
+    DATASET_ID = "bigq-etl.nasa"
+    TABLE_ID = "nasa_facilities"
+
+    sheet = BigQueryConnectedGoogleSheet(
+        sheet_url=SHEET_URL, dataset_id=DATASET_ID, table_id=TABLE_ID
+    )
+    sheet.create_table()
